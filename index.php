9cbe34d7528b8e54088c7dd8211c61fe3b745e16
carlos <radix_10339@hotmail.com>
2020-03-03 11:26:00 -0600

    add test url

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+use Pecee\SimpleRouter\SimpleRouter;
+
+require_once "vendor/autoload.php";
+
+SimpleRouter::get('/', function() {
+    return 'Hello world';
+});
+
+// Start the routing
+SimpleRouter::start();
