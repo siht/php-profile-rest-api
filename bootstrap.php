4e3731bf059af404f2863e4e741b7d64db4b408d
carlos <radix_10339@hotmail.com>
2020-03-03 01:58:42 -0600

    improve readabillity

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,8 @@
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//$dotenv->load();
 
 require_once "vendor/autoload.php";
 
@@ -13,11 +13,22 @@ $isDevMode = true;
 $proxyDir = null;
 $cache = null;
 $useSimpleAnnotationReader = false;
-$configuration_orm_metadata = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
+$configuration_orm_metadata = Setup::createAnnotationMetadataConfiguration(
+    array(
+        __DIR__."/src"
+    ),
+    $isDevMode,
+    $proxyDir,
+    $cache,
+    $useSimpleAnnotationReader
+);
 
 $db_connection_string = array(
     'url' => getenv('CONNECTION_STRING'),
 );
 
 // obtaining the entity manager
-$entityManager = EntityManager::create($db_connection_string, $configuration_orm_metadata);
+$entityManager = EntityManager::create(
+    $db_connection_string,
+    $configuration_orm_metadata
+);
