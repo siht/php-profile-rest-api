81255d9437de9fd720048d163e14bb66a0550789
carlos <radix_10339@hotmail.com>
2020-03-04 03:26:04 -0600

    use getProfiles in router

--- public/index.php
+++ public/index.php
@@ -3,9 +3,10 @@
 use Pecee\SimpleRouter\SimpleRouter;
 
 require_once "../vendor/autoload.php";
+require_once "../views.php";
 
-SimpleRouter::get('/', function() {
-    return 'Hello world';
+SimpleRouter::get('/', function(){
+    return getProfiles();
 });
 
 // Start the routing
