38b7dd217708a4282098ac1af3abdf481159b62a
carlos <radix_10339@hotmail.com>
2020-03-04 08:43:45 -0600

    now insertProfile works with json stream
    also was a refactor in code to improve readability

--- views.php
+++ views.php
@@ -7,30 +7,64 @@ function getProfiles(){
     $array_profiles = array();
 
     foreach ($profiles as $profile) {
-        $dict_profile = array(
-            'id' => $profile->getId(),
-            'image' => $profile->getImage(),
-            'title' => $profile->getTitle(),
-            'date' => $profile->getDate()
-        );
+        $dict_profile = objectProfileToDict($profile);
         array_push($array_profiles, $dict_profile);
     }
 
-    $json_profiles = json_encode(
-        $array_profiles,
-        JSON_UNESCAPED_SLASHES,
-        JSON_PRETTY_PRINT
-    );
+    $json_profiles = dictToPrettyJSON($array_profiles);
     return $json_profiles;
 }
 
 function insertProfile(){
-    require_once "bootstrap.php";
+    $entityBody = json_decode(stream_get_contents(detectRequestBody()), true);
+    $title = $entityBody['title'];
+    $image = $entityBody['image'];
+    $request_has_title_and_image = $title && $image;
+    if($request_has_title_and_image){ // both are required
+        require_once "bootstrap.php";
+        $profile = new Profile();
+        $profile->setTitle($title);
+        $profile->setImage($image);
+        $entityManager->persist($profile);
+        $entityManager->flush();
+        $dict_profile = objectProfileToDict($profile);
+        $json_profile = dictToPrettyJSON($dict_profile);
+        return $json_profile; // on success send the new object
+    }
+    // error messages if no title or image
+    $nor_title_nor_image = !$request_has_title_and_image;
+    $error_message = "";
+    if($nor_title_nor_image){
+        $error_message = "title and image are required";
+    }
+    else if(!$image){
+        $error_message = "image are required";
+    }
+    else if(!$title){
+        $error_message = "title are required";
+    }
+    $dict_error = array("error" => $error_message);
+    $json_error = dictToPrettyJSON($dict_error);
+    return $json_error;
+}
 
-    $profile = new Profile();
-    $profile->setTitle("titulo de prueba");
-    $profile->setImage("https://vanguardia.com.mx/sites/default/files/styles/paragraph_image_large_desktop_1x/public/homero-filosofo-5.jpg");
+function objectProfileToDict($profile){
+    return $dict_profile = array(
+        'id' => $profile->getId(),
+        'image' => $profile->getImage(),
+        'title' => $profile->getTitle(),
+        'date' => $profile->getDate()
+    );
+}
 
-    $entityManager->persist($profile);
-    $entityManager->flush();
+function dictToPrettyJSON($dict){
+    return json_encode($dict, JSON_UNESCAPED_SLASHES, JSON_PRETTY_PRINT);
+}
+
+function detectRequestBody() {
+    $rawInput = fopen('php://input', 'r');
+    $tempStream = fopen('php://temp', 'r+');
+    stream_copy_to_stream($rawInput, $tempStream);
+    rewind($tempStream);
+    return $tempStream;
 }
