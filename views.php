22ebdb18921e350c6380a676ac08a4df2314984d
carlos <radix_10339@hotmail.com>
2020-04-01 15:25:26 -0600

    add cors to getProfiles

--- views.php
+++ views.php
@@ -1,6 +1,9 @@
 <?php
 function getProfiles(){
     require_once "bootstrap.php";
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET");
     header('Content-Type: application/json');
     http_response_code(200);
     $profileRepository = $entityManager->getRepository('Profile');
